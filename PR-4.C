ebffaebb9937c6ac21182da86501a0516bc892d9
shreemadkumbhani <shreemadkumbhani@gmail.com>
2022-09-14 12:17:53 +0530

    c language

---
+++ PR-4.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+
+	printf(" - - -\n");
+	printf("|     |\n");
+	printf("S     |\n");
+	printf("H     |\n");
+	printf("R     |\n");
+	printf("E     |\n");
+	printf("E     |\n");
+	printf("M     |\n");
+	printf("A     |\n");
+	printf("D     |\n");
+	printf("|     |\n");
+	printf(" - - -\n");
+
+getch();
+
+}
